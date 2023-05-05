3288f8f9e280c41880b40d0d1f5b5f2bc6aee25d
이동율 <dy9623@naver.com>
2023-05-05 22:21:06 +0900

    [ac]#Week1 백준 2753

---
+++ 이동율/Week1/2753 윤년/2753 윤년/소스.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+	if (num % 4 == 0) {
+		if (num % 400 == 0 || num % 100 != 0) cout << "1";
+		else cout << "0";
+	}
+	else cout << "0";
+	return 0;
+}
