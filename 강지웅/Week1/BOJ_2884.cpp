485f69d57330749d5a4078dc7d04bedcdee682bc
KangJiUng <107825793+KangJiUng@users.noreply.github.com>
2023-05-06 18:48:25 +0900

    [ac]#Week1 백준 2884

---
+++ 강지웅/Week1/BOJ_2884.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int H, M;
+	cin >> H >> M;
+
+	if (M < 45) {
+		M += 15;
+		if (H != 0) {
+			H -= 1;
+		}
+		else {
+			H = 23;
+		}
+	}
+	else {
+		M -= 45;
+	}
+	cout << H << " " << M;
+	return 0;
+}
