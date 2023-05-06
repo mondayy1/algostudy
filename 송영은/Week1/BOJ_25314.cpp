f5999878c62d23689e5c50c4c48e6d3a2d9ecd19
Yeongeun Song <ye3557@naver.com>
2023-05-06 10:13:16 +0900

    [ac]BOJ_25314

---
+++ 송영은/Week1/BOJ_25314.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n;
+    k = n / 4;
+    for (int i = 0; i < k; i++)
+    {
+        cout << "long ";
+    }
+    cout << "int" << endl;
+    return 0;
+}
