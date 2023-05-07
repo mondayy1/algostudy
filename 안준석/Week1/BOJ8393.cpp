f4a5d3bed62cbe5af56ed010a9fc5f0c6d3d023d
Rahahhaaa <airlike7734@gmail.com>
2023-05-07 14:51:23 +0900

    [ac]#Week1 백준 8393

---
+++ 안준석/Week1/BOJ8393.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int ans = 0;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        ans += i;
+    }
+    cout << ans;
+    return 0;
+}
