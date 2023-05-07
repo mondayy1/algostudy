bd5356bc34aec36105531960446b89c3f5aa0a58
김기원 <>
2023-05-07 18:27:43 +0900

    [ac]#Week1 백준 9498

---
+++ 김기원/Week1/BOJ_9498.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A;
+    cin >> A;
+    
+    if (A >= 90) {
+        cout << "A" << endl;
+    } else if (A < 90 && A >= 80) {
+        cout << "B" << endl;
+    } else if (A < 80 && A >= 70) {
+        cout << "C" << endl;
+    } else if (A < 70 && A >= 60) {
+        cout << "D" << endl;
+    } else {
+        cout << "F" << endl;
+    }
+
+    return 0;
+}
