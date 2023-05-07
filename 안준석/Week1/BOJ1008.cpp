167f640cda29da59dbc40e2b06942756851d5a9e
Rahahhaaa <airlike7734@gmail.com>
2023-05-07 11:57:55 +0900

    [ac]#Week1 백준 1008

---
+++ 안준석/Week1/BOJ1008.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double a,b;
+    cin >> a >> b;
+
+    cout.precision(13);
+    cout << fixed;
+    cout << a / b;
+    return 0;
+}
