6b85315b5b2d5cfb52ad1e342b9a5dc2aa190711
Hyunjiiing <h.jilluminate@gmail.com>
2023-05-07 23:49:54 +0900

    [ac]#Week1 백준 1008

--- 오현지/Week1/BOJ_1008.cpp
+++ 오현지/Week1/BOJ_1008.cpp
@@ -6,6 +6,8 @@ int main() {
 	double ComputationalResults = 0;
 	cin >> A >> B;
 	ComputationalResults = (double)A / B;
+	cout << fixed;
+	cout.precision(9);
 	cout << ComputationalResults;
 
 	return 0;
