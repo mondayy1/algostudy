c314582fc93e406c0e041f0a9c8f72caab323f36
Hyunjiiing <h.jilluminate@gmail.com>
2023-05-07 23:02:17 +0900

    [wa]#Week1 백준 1008

---
+++ 오현지/Week1/BOJ_1008.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int A, B;
+	double ComputationalResults = 0;
+	cin >> A >> B;
+	ComputationalResults = (double)A / B;
+	cout << ComputationalResults;
+
+	return 0;
+}
