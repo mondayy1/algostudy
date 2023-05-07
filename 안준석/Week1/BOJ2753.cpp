fd661fd9788f90c0fbd10f6f1b943e281caead7a
Rahahhaaa <airlike7734@gmail.com>
2023-05-07 12:08:08 +0900

    [ac]#Week1 백준 2753

---
+++ 안준석/Week1/BOJ2753.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    if(n%400 == 0) cout << "1";
+    else if(n%4==0 && n%100 !=0) cout << "1";
+    else cout << "0";
+    return 0;
+}
