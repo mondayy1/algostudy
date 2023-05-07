e97ec3411484900ab29bae3c18aecc9b7e8d993f
Rahahhaaa <airlike7734@gmail.com>
2023-05-07 14:55:46 +0900

    [ac]#Week1 백준 25304

---
+++ 안준석/Week1/BOJ25304.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x,n,a,b;
+    cin >> x >> n;
+    for(int i=0;i<n;i++){
+        cin >> a >> b;
+        x -= a*b;
+    }
+    if(x==0) cout << "Yes";
+    else cout << "No";
+    return 0; 
+}
