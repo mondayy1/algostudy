3bba4c9156e42eab4d4cc2eef0c3bb149bc22a5d
gyseong <jsjs8667@naver.com>
2023-05-09 14:44:41 +0900

    [ac]Week2 백준 2438

---
+++ Week2/Week2/BOJ_2438.cpp
@@ -0,0 +1,20 @@
+//숫자를 입력받아 별 찍기 1
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j <= n - 1; j++) {
+			cout << " ";
+		}
+		for (int j = 1; j <= i  ; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
