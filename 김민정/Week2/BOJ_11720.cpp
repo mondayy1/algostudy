d8d7119584590c17b88b2903920e7fb985a2f946
minjeong kim <6alswjd6@gmail.com>
2023-05-11 20:34:39 +0900

    [ac]11720 string

---
+++ 김민정/Week2/BOJ_11720.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int n,x,sum=0;
+    string str;
+    cin >> n;
+    cin.ignore();
+    getline(cin,str);
+    for (int i=0;i<n;i++){
+        x = str[i] - '0';
+        sum += x;
+    }
+    cout << sum << endl;
+} //강제 (int)로 변환시 아스키코드 값이 나옴
+//https://11001.tistory.com/14
