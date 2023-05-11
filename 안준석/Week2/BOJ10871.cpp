ff8e89759fb0946d55552e08cba735ed5df028a8
Rahahhaaa <airlike7734@gmail.com>
2023-05-11 21:57:18 +0900

    [ac]#Week2 백준 10871

---
+++ 안준석/Week2/BOJ10871.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    int a;
+    for(int i=0;i<n;i++){
+        cin >> a;
+        if(a<x) cout << a << " ";
+    }
+}
