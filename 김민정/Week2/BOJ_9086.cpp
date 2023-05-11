87c51b79c78be67f38a559efc7f8d0793d0280ee
minjeong kim <6alswjd6@gmail.com>
2023-05-11 20:17:08 +0900

    [ac]9086 string

--- 김민정/Week2/BOJ_9086.cpp
+++ 김민정/Week2/BOJ_9086.cpp
@@ -6,7 +6,7 @@ int main(){
     string str;
     int n;
     cin >> n;
-    cin.ignore();
+    cin.ignore(); //해당 부분 https://novlog.tistory.com/entry/C-입력-함수-정리-cin-getline-feat-cinignore?category=885399 참고
     for (int i =0;i<n;i++){
         getline(cin,str);
         cout << str.front();
