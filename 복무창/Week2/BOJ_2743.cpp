39bc0f640b8095afed492b5e307c9850ae8e8e72
bokob <bmc0520@naver.com>
2023-05-12 11:59:07 +0900

    [ac]#Week2 백준 2743

---
+++ 복무창/Week2/BOJ_2743.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	string s;
+	getline(cin, s);
+
+	cout << s.length();
+
+	return 0;
+}
