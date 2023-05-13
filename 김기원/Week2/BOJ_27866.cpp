4117ce54a53eb3ac7ec6871d9b57942f416da37a
김기원 <>
2023-05-13 12:06:18 +0900

    [ac]#Week2 백준 27866

---
+++ 김기원/Week2/BOJ_27866.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string S;
+    int i;
+    
+    getline(cin, S);
+    cin >> i;
+    
+    cout << S[i - 1] << endl;
+    
+    return 0;
+}
