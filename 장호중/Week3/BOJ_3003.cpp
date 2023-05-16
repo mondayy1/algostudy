dfa55cffc82a471b62a6d9bf74c8079a0c8f245e
mondayy1 <hojoong310@gmail.com>
2023-05-16 22:03:45 +0900

    [ac]#Week3 백준 3003

---
+++ 장호중/Week3/BOJ_3003.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define all(a) a.begin(),a.end()
+#define endl '\n'
+#define ll long long
+#define pll pair<ll, ll>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    int a, b, c, d, e, f;
+
+    cin >> a >> b >> c >> d >> e >> f;
+
+    cout << 1 - a << ' ' << 1 - b << ' ' << 2 - c << ' ' << 2 - d << ' ' << 2 - e << ' ' << 8 - f;
+
+    return 0;
+}
