af5a2785ba0abfe00c3bf674e5a8be14c6ed737e
김준호 <kimjunho1231@naver.com>
2023-05-17 14:26:55 +0900

    [ac]#3 백준3003.cpp

---
+++ 김준호/Week3/BOJ_3003.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int P[6] = { 1,1,2,2,2,8 };
+	int arr[6] = { 0, };
+	int result[6] = { 0, };
+	for (int i = 0; i < 6; i++) {
+		cin >> arr[i];
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		result[i] = P[i] - arr[i];
+	}
+	for (int i = 0; i < 6; i++)
+	{
+		cout << result[i]<<" ";
+	}
+	return 0;
+}
