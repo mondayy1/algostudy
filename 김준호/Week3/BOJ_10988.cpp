2d83460177d6f629eef565dcdb35113236fd5eb9
김준호 <kimjunho1231@naver.com>
2023-05-17 20:07:23 +0900

    [ac]#3 백준10988.cpp

---
+++ 김준호/Week3/BOJ_10988.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+int main()
+{
+	//앞뒤 모두 똑같은 단어
+	string s;
+	cin >> s;
+	int t = s.size()-1;
+	for (int i = 0; i < s.size() / 2; i++)
+	{
+		if (s[i] == s[t])
+		{
+			t--;
+			continue;
+		}
+		else {
+			cout << 0;
+			return 0;
+		}
+	}
+	cout << 1;
+	return 0;
+}
