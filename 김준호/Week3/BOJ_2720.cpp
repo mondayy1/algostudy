da3350bfa07fb2c1ac26e369caae4e20a72ea012
김준호 <kimjunho1231@naver.com>
2023-05-17 13:42:37 +0900

    [ac]#3 백준 2720.cpp

---
+++ 김준호/Week3/BOJ_2720.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+using namespace std;
+void cal(int* arr,int t)
+{
+	int a[4] = { 25,10,5,1 };
+	int b[4] = { 0, };
+	for (int i = 0; i < t; i++)
+	{
+		for (int j = 0; j <4; j++)
+		{
+			b[j]=arr[i] / a[j];
+			arr[i] = arr[i] % a[j];
+
+
+		}
+
+		for (int q = 0; q < 4; q++)
+		{
+			cout << b[q] << " ";
+		}
+		cout << "\n";
+		for (int q = 0; q < 4; q++)
+		{
+			b[q] = 0;
+		}
+	}
+}
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(0);
+	int t;
+	cin >> t;
+	int* arr = new int[t];
+	for (int i = 0; i < t; i++)
+	{
+		cin >> arr[i];
+	}
+	cal(arr,t);
+	return 0;
+}
