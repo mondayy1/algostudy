f1ca5c70c457749fd42882352cc8e9fc63825ee8
kjh3291 <kjh51234@naver.com>
2023-05-19 14:50:51 +0900

    [ac] Week3 백준 3003

---
+++ 김재하/week3/BOJ_3003.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+  int K,Q,L,N,P,B;
+  cin >> K >> Q >> L >> B >> N >>P;
+  cout << 1-K << " " << 1-Q << " ";
+  cout << 2-L << " " << 2-B << " ";
+  cout << 2-N << " " << 8-P;
+}
