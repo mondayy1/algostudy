eef18fc0b3b59dcf4c9128beb8509f7196a4de99
kjh3291 <kjh51234@naver.com>
2023-05-19 14:43:52 +0900

    [ac] Week3 백준 5086

---
+++ 김재하/week3/BOJ_5086.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n1,n2;
+  
+  while(1)
+    {
+      cin >> n1 >> n2;
+      if(n1 == 0&& n2 == 0) return 0;
+      if(n2%n1==0) cout << "factor\n";
+      else if(n1%n2==0) cout << "multiple\n";
+      else cout << "neither\n";
+    }
+  
+}
