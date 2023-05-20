feb6648a39dff37ec0aaf8b197e7825987f905db
yanghaemi <heau0105@naver.com>
2023-05-21 00:42:31 +0900

    [ac]Week3 BOJ_10988

---
+++ 양해미/Week 3/BOJ_10988.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    string str;
+    cin >> str;
+
+    for (int i = 0; i < str.length() / 2; i++)
+    {
+        if (str[i] != str[str.length() - i - 1])
+        {
+            cout << "0";
+            return 0;
+        }
+    }
+    cout << "1";
+}
