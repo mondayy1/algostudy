0e648ee1a32d04b6e112d02287dac75d00564e4b
kmingseo <128784989+kmingseo@users.noreply.github.com>
2023-05-20 14:53:21 +0900

    [ac]#Week3 백준 10988

---
+++ 김민서/BOJ_10988.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+
+    string str;
+    
+
+    cin >> str;
+    int len= str.length();
+    
+
+    for(int i=0;i<len/2;i++){
+        if(str[i]!=str[str.length()-1-i]){
+            cout <<"0"<<"\n";
+            return 0;
+        }
+    }
+    cout<<"1"<<"\n";
+
+    
+    
+}
