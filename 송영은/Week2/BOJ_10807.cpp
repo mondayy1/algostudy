b743d763693d75e8ee68dda4193c1c882ef2454e
Yeongeun Song <ye3557@naver.com>
2023-05-20 13:07:53 +0900

    [ac]

---
+++ 송영은/Week2/BOJ_10807.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> vec(100);
+    int n, v, temp, count = 0;
+
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec.at(i);
+        // vec.push_back(temp);
+    }
+    cin >> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (vec[i] == v)
+            count += 1;
+    }
+    cout << count;
+}
