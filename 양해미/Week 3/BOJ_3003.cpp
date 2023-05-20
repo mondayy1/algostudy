586b6d2eba062301487a4b391a5a594d616583be
yanghaemi <heau0105@naver.com>
2023-05-21 00:28:40 +0900

    [ac]Week3 BOJ_3003

---
+++ 양해미/Week 3/BOJ_3003.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int k, q, l, b, kn, p = 0;
+    cin >> k >> q >> l >> b >> kn >> p;
+
+    int x = 0;
+
+    cout << 1 - k << " " << 1 - q << " " << 2 - l << " " << 2 - b << " " << 2 - kn << " " << 8 - p;
+}
