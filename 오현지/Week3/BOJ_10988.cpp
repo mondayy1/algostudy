344fbcb279367cd2c05200d07e1faa2e447fa93e
Hyunjiiing <h.jilluminate@gmail.com>
2023-05-21 12:34:48 +0900

    [ac]#Week3 백준 10988

--- 오현지/Week3/BOJ_10988.cpp
+++ 오현지/Week3/BOJ_10988.cpp
@@ -1,4 +1,5 @@
-/* 1차시도 실패(9분 21초) : 이것도 for문 조건 지정 / string함수 잘 모름
+/* 역문자열로 해도 같은 문자인지 확인
+소요시간 : 1시간 이상
 */
 #include <iostream>
 #include <string>
@@ -7,14 +8,12 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	string S;
-	string reverseS;
+	string reverseS = "";
 	cin >> S;
 	int length = S.length();
-	for (int i = length; i >= 0; i--) {
-		int j = 0;
-		reverseS[i] = S[j];
-		j++;
+	for (int i = length - 1; i >= 0; i--) {
+		reverseS += S[i];
 	}
-	if (S == reverseS) cout << 1;
+	if (S.compare(reverseS) == 0) cout << 1;
 	else cout << 0;
 }
