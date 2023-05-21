fe307c51bb4c59c711951a97edd9c2840a891aa1
Hyunjiiing <h.jilluminate@gmail.com>
2023-05-21 12:02:14 +0900

    [ac]#Week3 백준 3003

--- 오현지/Week3/BOJ_3003.cpp
+++ 오현지/Week3/BOJ_3003.cpp
@@ -1,37 +1,20 @@
-//1차시도 실패(15분) : 런타임 에러 <- for문 조건문 잘못한것으로 추측
-
+/*체스 갯수
+소요시간 : 1시간 이상
+*/
 #include <iostream>
 using namespace std;
 int main() {
-	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	int peice[6] = { 0,0,0,0,0,0 };
-	int count[6] = { 0,0,0,0,0,0 };
-	int after_peice[6] = { 1,1,2,2,2,8 };
-
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
+	cin.tie(0);
+	int before_arr[6] = { 0 };
+	int after_arr[6] = { 1, 1, 2, 2, 2, 8 };
+	int difference_arr[6] = { 0 };
 	for (int i = 0; i < 6; i++) {
-		cin >> peice[i];
-		if (peice[i] > after_peice[i]) {
-			for (int j = 1; peice[i] >= after_peice[i]; j++) {
-				peice[j]--;
-				count[j] = -j;
-			}
-		}
-		else if (peice[i] < after_peice[i]) {
-			for (int j = 1; peice[i] <= after_peice[i]; j++) {
-				peice[j]++;
-				count[j] = j;
-			}
-		}
-		else continue;
-
+		cin >> before_arr[i];
+		difference_arr[i] = after_arr[i] - before_arr[i];
 	}
 	for (int i = 0; i < 6; i++) {
-		cout << count[i] << ' ';
+		cout << difference_arr[i] << ' ';
 	}
-
-
+	return 0;
 }
